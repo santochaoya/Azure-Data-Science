98cacd69c2942eeba697e5f75fc6f83254a08409
Xiao_Meng <santochaoya@yahoo.com.hk>
2021-11-04 11:29:45 +0800

    update script as experiment

---
+++ src/SDK/run.py
@@ -0,0 +1,24 @@
+from azureml.core import Experiment, ScriptRunConfig, Environment, Workspace
+from azureml.core.conda_dependencies import CondaDependencies
+
+
+# Create a python environment
+sklearn_env = Environment('sklearn-env')
+
+# Ensure the required packages are installed
+packages = CondaDependencies.create(pip_packages=['azureml-defaults','scikit-learn'])
+sklearn_env.python.conda_dependencies = packages
+
+# Create a script config
+script_config = ScriptRunConfig(source_directory='src/SDK/training',
+                                script='training.py',
+                                environment=sklearn_env)
+
+# Submit the experiment
+ws = Workspace.from_config()
+
+experiment_name = 'SDK-exercise'
+experiment = Experiment(workspace=ws, name=experiment_name)
+run = experiment.submit(config=script_config)
+# run.wait_for_completion()
+
--- src/SDK/training/training.py
+++ src/SDK/training/training.py
@@ -1,2 +1,37 @@
-from azureml.core import RunConfiguration
+from azureml.core import Run
 import pandas as pd
+import numpy as np
+import joblib
+from scipy.sparse.construct import rand
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+
+# Get the experiment context
+run = Run.get_context()
+
+# Get the training model
+data = pd.read_csv('data/wine.csv')
+data['WineVariety'] = data['WineVariety'].replace(2, 1)
+
+X, y = data[['Alcohol', 'Malic_acid']], data['WineVariety']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+reg = 0.1
+model = LogisticRegression(C=1/reg, solver='liblinear')
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+
+# log the accurancy
+run.log('Accuracy', np.float(np.average(y_pred==y_test)))
+
+# Save the model
+joblib.dump(model, filename='outputs/model.pkl')
+
+# Complete the experiment
+run.complete()
