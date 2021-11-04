4ea6ff9a54acceac3873f97d2b6485c0ad07b337
Xiao_Meng <santochaoya@yahoo.com.hk>
2021-11-04 15:23:53 +0800

    SDK script as experiment

--- src/SDK/run.py
+++ src/SDK/run.py
@@ -6,7 +6,8 @@ from azureml.core.conda_dependencies import CondaDependencies
 sklearn_env = Environment('sklearn-env')
 
 # Ensure the required packages are installed
-packages = CondaDependencies.create(pip_packages=['azureml-defaults','scikit-learn'])
+packages = CondaDependencies.create(conda_packages=['scikit-learn', 'pip'],
+                                    pip_packages=['azureml-defaults','scikit-learn'])
 sklearn_env.python.conda_dependencies = packages
 
 # Create a script config
@@ -22,3 +23,7 @@ experiment = Experiment(workspace=ws, name=experiment_name)
 run = experiment.submit(config=script_config)
 run.complete()
 
+for file in run.get_file_names():
+    print(file)
+
+    run.download_file(name=file, output_file_path='outputs/log.txt')
